5fbe6f34aa9a6b727cf924065f570aec105f370a
commit name <dydgh1095@naver.com>
2022-09-14 14:33:19 +0900

    #6-스프링 부트 프로젝트 시작
    사용한 초기 기술
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/test/java/com/project/pr_board/PrBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pr_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
