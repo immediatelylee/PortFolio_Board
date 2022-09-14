12184b32c27aae181a47b476d08f7d906eeb1035
commit name <dydgh1095@naver.com>
2022-09-14 17:25:09 +0900

    #6 -  스프링부트 위치 재조정
    스프링부트의 open위치가 git영역이 아니여서 다시 생성함

---
+++ src/main/java/com/project/pr_project/PrProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.pr_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/pr_project/PrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pr_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
