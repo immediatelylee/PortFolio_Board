5c90fa8ead00f63cee4a31f65d29e37b20aafcbc
commit name <dydgh1095@naver.com>
2022-12-11 21:43:52 +0900

    #21 - 댓글 서비스 로직의 테스트와 골격 잡기 이후 업데이트
    원래는 댓글 서비스 로직의 테스트와 골격을 잡고
    커밋 후 업데이트를 진행하려고 하였으나
    이미 업데이트를 진행하였으므로 최종본으로 커밋 함
    어떤 방식으로 골격을 잡았는지는 따로 정리할 것이다.
    
    - 기존 골격에서 DTO부분을 재설계하였음. 
    - 테스트가 필요로 하는 repository 쿼리 메소드도 작성

---
+++ src/main/java/com/project/pr_project/dto/ArticleCommentDto.java
@@ -0,0 +1,48 @@
+package com.project.pr_project.dto;
+
+import com.project.pr_project.domain.Article;
+import com.project.pr_project.domain.ArticleComment;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@EqualsAndHashCode
+@Getter
+public class ArticleCommentDto {
+    private final Long id;
+    private final Long articleId;
+    private final UserAccountDto userAccountDto;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private final String createdBy;
+    private final LocalDateTime modifiedAt;
+    private final String modifiedBy;
+
+
+    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleCommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+    public static ArticleCommentDto from(ArticleComment entity) {
+        return new ArticleCommentDto(
+                entity.getId(),
+                entity.getArticle().getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getContent(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public ArticleComment toEntity(Article entity) {
+        return ArticleComment.of(
+                entity,
+                userAccountDto.toEntity(),
+                content
+        );
+    }
+}
---
+++ src/main/java/com/project/pr_project/service/ArticleCommentService.java
@@ -0,0 +1,32 @@
+package com.project.pr_project.service;
+
+import com.project.pr_project.dto.ArticleCommentDto;
+import com.project.pr_project.repository.ArticleCommentRepository;
+import com.project.pr_project.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleCommentRepository articleCommentRepository;
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComments(long articleId) {
+        return List.of();
+    }
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void updateArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
+    }
+}
